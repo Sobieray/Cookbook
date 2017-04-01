51ab9bbb862ccec6d9a6583fef829a6e37e127c5
Matthew Sobieray <matt@hyfyn.com>
2017-04-01 00:55:45 -0600

    update node to get data from db and vue to fetch the data from the node api

--- node/routes/users.js
+++ node/routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
+var db = require('../db');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  db('users').then((users) => {
+		res.send(users)
+			//;
+	}, next);
+  
 });
 
 module.exports = router;
